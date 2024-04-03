300968f7d03bea1660b5f4635fc0e319c7d649c9
MuhammadMuzammilkhan <115549730+MuhammadMuzammilkhan@users.noreply.github.com>
2024-04-03 20:02:57 +0500

    Singleton pattern practice.

---
+++ Myproject/src/App.java
@@ -0,0 +1,12 @@
+// import java.util.Scanner;
+
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+//         Scanner inputScanner=new Scanner(System.in);
+//         System.out.println("pleaze enter your name");
+//         String name= inputScanner.nextLine();
+//         System.out.println("Hello"+name);
+//         inputScanner.close();
+//     }
+// }
